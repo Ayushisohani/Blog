5aa2a50d148ef8a05612547ec9c6395b19a3030e
Ayushisohani <aayushisohani01@gmail.com>
2024-09-02 20:48:03 +0530

    Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import Logo from '../logo'
 function Footer() {
   return (
     <section className="relative overflow-hidden py-10 bg-blue-400 ">
-            <div className="relative z-10 mx-auto max-w-5xl px-4">
+            <div className="relative z-10 mx-auto max-w-4xl px-4">
                 <div className="-m-6 flex flex-wrap">
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                         <div className="flex h-full flex-col justify-between">
